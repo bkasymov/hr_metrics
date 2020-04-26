a3974e8a2b87543a9e51a52bbbae1f81526af23b
dpenney <behaqosimi@gmail.com>
2020-04-26 13:00:50 +0300

    cool!

---
+++ Makefile
@@ -0,0 +1,8 @@
+NAME = metrics
+
+SRC = ft_atoi.c Middle_age_of_colleges.c
+
+all: (NAME)
+
+NAME:
+	gcc $(SRC) 
---
+++ cmake-build-debug/CMakeFiles/hr_metrics.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hr_metrics.dir/Middle_age_of_colleges.c.o"
+  "hr_metrics"
+  "hr_metrics.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hr_metrics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
